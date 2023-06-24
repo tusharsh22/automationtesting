23a859ca40628f7a07369ce533b4bdc99860e383
tusharsh22 <tushar.sharma4477@gmail.com>
2023-06-24 13:08:53 +0530

    initial commit

---
+++ src/knowledge/company/day05.java
@@ -0,0 +1,10 @@
+package knowledge.company;
+
+public class day05 {
+    public static void main(String[] args) {
+        int i;
+        for ( i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/knowledge/company/methods.java
@@ -0,0 +1,25 @@
+package knowledge.company;
+
+public class methods {
+
+    static int logic(int x, int y){
+        int z;
+        z=x+y;
+        return z;
+
+    }
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b= 5;
+        int c;
+        c=logic(a,b);
+        int a1=8;
+        int b1=6;
+        int c1;
+        c1=logic (a1,b1);
+        System.out.println(c);
+        System.out.println(c1);
+
+    }
+}
